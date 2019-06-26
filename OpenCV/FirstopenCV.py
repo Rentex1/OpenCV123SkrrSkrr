df6d381fa96e6aca368dd8d61934ed4b8a652304
Rentex1 <luis.stauss@gmx.de>
2019-06-26 15:43:05 +0200

    We have done it

--- OpenCV/FirstopenCV.py
+++ OpenCV/FirstopenCV.py
@@ -2,11 +2,17 @@ import numpy as np
 import cv2 
 import matplotlib as plt
 
-Frau = cv2.imread('TestFrau.jpg')
+Frau = cv2.imread('3TestFrauen.jpg')
+grayFrau = cv2.cvtColor(Frau, cv2.COLOR_BGR2GRAY)
+faceHaar = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+faces = faceHaar.detectMultiScale(grayFrau, scaleFactor=1.1, minNeighbors=5)
 
-#while True:
+
+print('Faces found: ', len(faces))
+cv2.imshow('grayFrau', grayFrau) 
 
 
-cv2.imshow('Test Imag', Frau) 
+
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
