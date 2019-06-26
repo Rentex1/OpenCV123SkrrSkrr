fd893d2a757672db12ca288fba226584cac83b83
Rentex1 <luis.stauss@gmx.de>
2019-06-26 21:52:02 +0200

    Update FirstopenCV.py

--- OpenCV/FirstopenCV.py
+++ OpenCV/FirstopenCV.py
@@ -2,13 +2,13 @@ import numpy as np
 import cv2 
 import matplotlib as plt
 
-Frau = cv2.imread('3TestFrauen.jpg')
+Frau = cv2.imread('TestFrau.jpg')
 grayFrau = cv2.cvtColor(Frau, cv2.COLOR_BGR2GRAY)
 faceHaar = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 faces = faceHaar.detectMultiScale(grayFrau, scaleFactor=1.1, minNeighbors=5)
 
 
-print('Faces found: ', len(faces))
+print('Frauen found: ', len(faces))
 cv2.imshow('grayFrau', grayFrau) 
 
 
