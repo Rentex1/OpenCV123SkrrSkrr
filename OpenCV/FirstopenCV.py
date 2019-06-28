a84b79fa206783749b3e7b9a3ede9f8737d47e5d
Rentex1 <luis.stauss@gmx.de>
2019-06-28 20:56:02 +0200

    Fräuleihn

--- OpenCV/FirstopenCV.py
+++ OpenCV/FirstopenCV.py
@@ -1,18 +1,19 @@
 import numpy as np
-import cv2 
+import cv2 as cv2
 import matplotlib as plt
 
-Frau = cv2.imread('TestFrau.jpg')
+Frau = cv2.imread('3Frauen.jpg')
 grayFrau = cv2.cvtColor(Frau, cv2.COLOR_BGR2GRAY)
 faceHaar = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 faces = faceHaar.detectMultiScale(grayFrau, scaleFactor=1.1, minNeighbors=5)
 
-
+for (x, y, w, h) in faces:
+    cv2.rectangle(Frau, (x, y), (x+w, y+h), (0, 255, 0), 3)
 print('Frauen found: ', len(faces))
-cv2.imshow('grayFrau', grayFrau) 
+cv2.imshow('Frau', Frau) 
 
 
 
 
-cv2.waitKey(0)
+cv2.waitKey(5000)
 cv2.destroyAllWindows()
